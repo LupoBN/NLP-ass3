5ce92a6c5df83f95b364a99617da72bce8081a8e
shauli1321 <shauli321@gmail.com>
2017-12-19 15:06:44 +0200

    .

--- Main.py
+++ Main.py
@@ -1,14 +1,32 @@
+from Strategy import *
+import sys
 from collections import defaultdict, Counter
 
 def read_file(file_name):
-    input_file = open(file_name, 'r')
-    sentences = list()
-    sent = []
-    lines = input_file.readlines()
-    setnences = "".join(lines).split("\r\n\r\n")
 
-    input_file.close()
-    return sentences
+    with open(file_name, "r") as f :
+        lines = f.readlines()
+        print ("size: ", sys.getsizeof(lines) / 1e6)
+        print ("length: ", len(lines))
+        print ("last: ", lines[-2])
+
+        lines = [line.replace("\r\n", "") for line in lines]
+        sentences, sentence = [], []
+
+        for line in lines:
+            #print line
+            if len(line) > 0:
+                sentence.append(line)
+            else:
+                sentences.append(sentence)
+                sentence = []
+
+        return sentences
+
+
 if __name__ == "__main__":
-    k = read_file("wikipedia.sample.trees.lemmatized")
-    pass
+    sentences = read_file("wikipedia.sample.trees.lemmatized")
+    print ("size of sentences:", sys.getsizeof(sentences)/1e6)
+    print (len(sentences))
+    s = WindowContextWord()
+    words, context = s.get_context(sentences[0])
